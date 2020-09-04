9a822b21de08899c63aec37f1ddc3c962ed1cfaa
James Lamb <jameslamb13@gmail.com>
2020-09-03 20:31:11 -0400

    updated booleans and cleaned up js.

--- script.js
+++ script.js
@@ -11,96 +11,65 @@ function writePassword() {
 }
 
 
-
-
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var special = ["!", "#", "$", "%", "&", "*", "+", "-", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\",", "]", "^", "_", "`", "{", "}", "~", " "];
+var special = ["!", "#", "$", "%", "&", "*", "+", "-", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "}", "~"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-// var userChoices = [number, special, upperCase, lowerCase]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 
-
-
-var randomNumb = number[Math.floor(Math.random() * number.length)];
-var randomSpec = special[Math.floor(Math.random() * special.length)];
-var randomUpper = upperCase[Math.floor(Math.random() * upperCase.length)];
-var randomLower = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-
-
-
-
-function generatePassword(length, userChoices) {
-  randomPass = "";
+function generatePassword() {
+  listOfChar = []
   // prompt length
+  
   var length = parseInt(prompt("Pick a number between 8 and 128 for the length of your password."));
   while (length < 8 || length > 128) {
     alert("please choose a valid number");
-    i--;
+    var length = parseInt(prompt("Pick a number between 8 and 128 for the length of your password."));
   }
-  // userChoices
+
+  // confirm the use of numbers
+   var hasNumb = confirm("Do you want to use numbers?");
+  if (hasNumb === true) { listOfChar = listOfChar.concat(number) };
+ 
   // confirm the use of special characters
-  var special = confirm("Do you want to use special characters?");
-  // get a number
+  var hasSpecial = confirm("Do you want to use special characters?");
+  if (hasSpecial === true) { listOfChar = listOfChar.concat(special) };
+
   // confirm the use of upperCase
-  var upperCase = confirm("Do you want to use upper case?");
+  var hasUpperCase = confirm("Do you want to use upper case?");
+  if (hasUpperCase === true) { listOfChar = listOfChar.concat(upperCase) };
+
   // confirm the use of lowerCase
-  var lowerCase = confirm("Do you want to use lower case?");
-  // for (i = 0; i < length; i++)
-    // take the value the user gives and the true confirms
-    // and then randomly generate password up to the amount the user gives 
-    // i == userNumber; 
-    // alert if they didnt pick anything
-    // var pass = "";
-    var userChoices = {
-      numberSpec: number.concat(special),
+  var hasLowerCase = confirm("Do you want to use lower case?");
+  if (hasLowerCase === true) { listOfChar = listOfChar.concat(lowerCase) }; 
+  
+  if (!hasLowerCase && !hasUpperCase && !hasSpecial && !hasNumb) {
+    alert("You need to select at least one character");
+    location.reload();
+  
+    
+  }
+  
 
-      numberUpper: number.concat(upperCase),
-
-      numberLower: number.concat(lowerCase),
-
-      specUpper: special.concat(upperCase),
-
-      specLower: special.concat(lowerCase),
-
-      upperLower: upperCase.concat(lowerCase),
-
-      notNumber: special.concat(upperCase.lowerCase),
-
-      notSpecial: number.concat(uppeCase.lowerCase),
-
-      notUpper: number.concat(special.lowerCase),
-
-      notLower: number.concat(special.upperCase),
-
-      allTypes: number.concat(special.upperCase.lowerCase),
-    }
+  // take the value the user gives and the true confirms
+  // and then randomly generate password up to the amount the user gives 
 
 
 
+  var randomPass = ""
+  for (var i = 0; i < length; i++) {
+    randomPass += listOfChar[Math.floor(Math.random() * listOfChar.length)];
+
+  }
+
+
+
+  return randomPass;
 
 }
+// alert if they didnt pick anything
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// return pass
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
